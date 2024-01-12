00b44fec6598333f915f7fa1c489914e85ec5ddc
HAMMER17 <plutalov70@gmail.com>
2024-01-12 12:31:04 +0300

    repit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    // serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+    // serverActionsBodySizeLimit: "4mb"
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+    ],
+
+  },
 }
 
 module.exports = nextConfig
