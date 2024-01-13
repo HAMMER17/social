6dded076f5ea613d6cbac57e182e34d0e3adc008
HAMMER17 <plutalov70@gmail.com>
2024-01-13 15:59:46 +0300

    bg

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -8,7 +8,7 @@ import { dark } from "@clerk/themes";
 
 const LeftSideBar = () => {
   return (
-    <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
+    <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar bg-black">
       <Link href="/">
         <Image src={hat} alt="logo" width={200} height={200} />
       </Link>
